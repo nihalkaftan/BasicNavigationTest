dabea0c123703134ca5b661ee51826ea7ad5f8be
nihalkaftan <nihalkaftan@gmail.com>
2020-05-12 19:21:31 -0400

    Homework 4

--- src/test/java/com/cbt/tests/randomTests/Links.java
+++ src/test/java/com/cbt/tests/randomTests/Links.java
@@ -1,12 +1,38 @@
 package com.cbt.tests.randomTests;
 
 import com.cbt.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Links {
 
-    public void setup(){
-        Driver.getDriver("chrome").get("https://www.w3schools.com/");
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(),15);
 
+    @BeforeMethod
+    public void setup(){
+        Driver.getDriver().get("https://www.w3schools.com/");
+    }
+
+    @Test
+    public void test(){
+        List<WebElement> links = Driver.getDriver().findElements(By.xpath("//a"));
+        for (int i = 0; i < links.size() ; i++) {
+            if (links.get(i).isDisplayed()){
+                System.out.println(links.get(i).getText()+ " --> "+links.get(i).getAttribute("href"));
+            }
+        }
+    }
+
+    @AfterMethod
+    public void teardown(){
+        Driver.closeDriver();
     }
 
 }
--- src/test/java/com/cbt/utilities/Driver.java
+++ src/test/java/com/cbt/utilities/Driver.java
@@ -3,8 +3,6 @@ package com.cbt.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 
@@ -12,28 +10,10 @@ public class Driver {
 
     public Driver(){}
 
-    public static WebDriver getDriver(String driverType){
-        switch (driverType){
-            case "chrome":
-                WebDriverManager.chromedriver().version("79").setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--start-maximized");
-                driver = new ChromeDriver(chromeOptions);
-                break;
-            case "chromeHeadless":
-                //to run chrome without interface (headless mode)
-                WebDriverManager.chromedriver().version("79").setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--start-maximized");
-                options.setHeadless(true);
-                driver = new ChromeDriver(options);
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().version("79").setup();
-                driver = new FirefoxDriver();
-                break;
-            default:
-                throw new RuntimeException("Wrong browser name!");
+    public static WebDriver getDriver(){
+        if(driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
         }
         return driver;
     }
